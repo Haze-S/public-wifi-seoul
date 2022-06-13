eb0ee084604b51e0874a4ecc96e2432c36a52907
haze <skdml20022@gmail.com>
2022-06-14 00:12:44 +0900

    entity

---
+++ src/main/java/com/example/web/entity/HistWifi.java
@@ -0,0 +1,10 @@
+package com.example.web.entity;
+
+import java.util.Date;
+
+public class HistWifi {
+    private int histNo;
+    private double longitude;
+    private double latitude;
+    private Date searchDate;
+}
---
+++ src/main/java/com/example/web/entity/PubWifi.java
@@ -0,0 +1,22 @@
+package com.example.web.entity;
+
+import java.time.Year;
+
+public class PubWifi {
+    private String mgrNo;
+    private String region;
+    private String mainNm;
+    private String address;
+    private String addressDetail;
+    private String installFloor;
+    private String installTy;
+    private String installMby;
+    private String serviceSe;
+    private String networkTy;
+    private int installYear;
+    private boolean isOutdoor;
+    private String connectEnv;
+    private double longitude;
+    private double latitude;
+    private String workDate;
+}
